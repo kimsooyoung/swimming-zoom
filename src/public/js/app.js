aee8236bee024e7ba380acbc899df8bca5ada7ab
swimming_kim <tge1375@naver.com>
2021-09-18 18:43:05 +0900

    Websocket Messages
    Signed-off-by: swimming_kim <tge1375@naver.com>

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,17 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
 
+socket.addEventListener("open", () =>{
+    console.log("Connected to Server ✅");
+})
 
+socket.addEventListener("message", (msg) =>{
+    console.log(`I just got : ${msg.data}`);
+})
+
+socket.addEventListener("close", () =>{
+    console.log("Disconnected to Server ❌");
+})
+
+setTimeout(()=>{
+    socket.send("Hello I'm Brower");
+}, 3000);
