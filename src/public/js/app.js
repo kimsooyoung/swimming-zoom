1b6dbedfee548854b54f598354c185fdb657fa0d
swimming_kim <tge1375@naver.com>
2021-09-19 12:51:37 +0900

    SocketIO is Amazing
    Signed-off-by: swimming_kim <tge1375@naver.com>

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,16 @@
 const socket = io();
+
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+
+function handleRoomSubmit(event) {
+  event.preventDefault();
+  const input = form.querySelector("input");
+  console.log({ payload: input.value });
+  socket.emit("enter_room", { payload: input.value }, () => {
+    console.log("Done");
+  });
+  input.value = "";
+}
+
+form.addEventListener("submit", handleRoomSubmit);
