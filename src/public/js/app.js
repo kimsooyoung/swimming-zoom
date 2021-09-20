f42374c899b6b5ca06d6fbc039c53e6dcc367a73
swimming_kim <tge1375@naver.com>
2021-09-20 14:48:52 +0900

    Call Controls
    Signed-off-by: swimming_kim <tge1375@naver.com>

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,10 +3,26 @@ const socket = io();
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const cameraSelect = document.getElementById("cameras");
 
 let myStream;
 let muted = true;
 let cameraOff = true;
+
+async function getCameras() {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.innerText = camera.label;
+      option.value = camera.value;
+      cameraSelect.appendChild(option);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
 
 async function getMedia() {
   try {
@@ -15,6 +31,7 @@ async function getMedia() {
       video: true,
     });
     myFace.srcObject = myStream;
+    await getCameras();
   } catch (e) {
     console.log(e);
   }
@@ -23,6 +40,9 @@ async function getMedia() {
 getMedia();
 
 function handleMuteClick() {
+  myStream
+    .getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
   if (!muted) {
     muteBtn.innerText = "Mute";
     muted = true;
@@ -33,11 +53,14 @@ function handleMuteClick() {
 }
 
 function handleCameraClick() {
+  myStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
   if (cameraOff) {
-    cameraBtn.innerText = "Turn Camera Off";
+    cameraBtn.innerText = "Turn Camera On";
     cameraOff = false;
   } else {
-    cameraBtn.innerText = "Turn Camera On";
+    cameraBtn.innerText = "Turn Camera Off";
     cameraOff = true;
   }
 }
